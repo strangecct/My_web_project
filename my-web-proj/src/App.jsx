4d635a2fc6a73bf86cba6d9915a39b3eb259be82
qinzhe <1750445017@qq.com>
2023-03-21 15:09:12 +0800

    NavBar完成

---
+++ my-web-proj/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MyNavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavBar />
+    </div>
+  );
+}
+
+export default App;
